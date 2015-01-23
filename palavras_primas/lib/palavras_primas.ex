1abcbaf6be3a1e7871d3160800bbdf49ef8dd770
Ottony Costa <ottony93@gmail.com>
2015-01-22 23:49:27 -0300

    prime words

--- palavras_primas/lib/palavras_primas.ex
+++ palavras_primas/lib/palavras_primas.ex
@@ -1,2 +1,19 @@
 defmodule PalavrasPrimas do
+  def to_int_list word do
+    word |> String.to_char_list |>
+    Enum.map fn
+      x when x < 97 -> x - 38
+      x -> x - 96
+    end
+  end
+
+  def to_int word do
+    word |> to_int_list |> Enum.reduce fn
+      x, acc -> acc + x
+    end
+  end
+
+  def prime? word do
+    word |> to_int |> Primes.prime?
+  end
 end
--- palavras_primas/test/palavras_primas_test.exs
+++ palavras_primas/test/palavras_primas_test.exs
@@ -1,7 +1,50 @@
 defmodule PalavrasPrimasTest do
   use ExUnit.Case
 
+  import PalavrasPrimas
+
   test "the truth" do
     assert 1 + 1 == 2
+  end
+
+  test "represent a by 1" do
+      assert "a" |> to_int_list == [1]
+  end
+
+  test "represent A by 27" do
+    assert "A" |> to_int_list == [27]
+  end
+
+  test "represent z by 26" do
+    assert "z" |> to_int_list == [26]
+  end
+
+  test "represent Z by 54" do
+    assert "Z" |> to_int_list == [52]
+  end
+
+  test "represent sequence of letters" do
+    expected = [1, 2, 3, 4, 27, 28, 29 , 30]
+    assert "abcdABCD" |> to_int_list == expected
+  end
+
+  test "convert 'abcd' to integer" do
+    assert "abcd" |> to_int == 1+2+3+4
+  end
+
+  test "convert 'AbCd' to integer" do
+    assert "AbCd" |> to_int == 27+2+29+4
+  end
+
+  test "'abcd' is not prime" do
+    refute "abcd" |> prime?
+  end
+
+  test "'aabcd' is prime" do
+    assert "aabcd" |> prime?
+  end
+
+  test "to finish, 'ottony' is prime word" do
+    assert "ottony" |> prime?
   end
 end
